7cf69ec14e77248e25838b06f00bb03aebf51b90
joelramsey <joel.d.ramsey@gmail.com>
2015-04-15 15:17:00 +0000

    Adjustments to the item form to space out the tasks vertically

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -5,63 +5,67 @@ class ItemsController < ApplicationController
     authorize @items
   end
   
-  def show
-    @user = current_user
-    @item = Item.find(params[:id])
-    authorize @item
-  end
+  #def show  
+   # @user = current_user
+   # @item = Item.find(params[:id])
+   # authorize @item
+  #end
   
   def new
+    
     @user = current_user
     @item = Item.new
     authorize @item
   end
   
   def create
+   
     @user = current_user
     @item = current_user.items.build(item_params)
     authorize @item
+    
     if @item.save(item_params)
       flash[:notice] = "Item was successfully created."
-      redirect_to @item
+      redirect_to action: 'index'
       
     else
       flash[:alert] = "Error creating item."
-      redirect_to @item
+      redirect_to action: 'index'
     end
   end
   
   def edit
+    
     @user = current_user
     @item = Item.find(params[:id])
     authorize @item
   end
   
   def update
+ 
     @user = current_user
-    @item = Item.new(item_params)
+    @item = Item.find(params[:id])
     authorize @item
+    
     if @item.update_attributes(item_params)
       flash[:notice] = "Item was successfully updated."
-      redirect_to @item
+      redirect_to action: 'index'
       
     else
       flash[:alert] = "Error updating item."
-      redirect_to @item
+      redirect_to action: 'index'
     end
   end
   
   def destroy
     @item = Item.find(params[:id])
-    @items = current_user.items
-    name = @item.name
     authorize @item
     
     if @item.destroy
-      flash[:notice] = "\"#{name}\" was deleted successfully."
+      flash[:notice] = "Task complete!"
       
     else
-      flash[:error] = "There was an error deleting the item."
+      flash[:error] = "There was an error deleting the task. Please try again."
      
     end
     respond_to do |format|
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,7 +1,7 @@
 class Item < ActiveRecord::Base
   belongs_to :user
   validates :name, length: {minimum: 6}, presence: true
-  validates :details, length: {minimum: 10}, presence: true
+  
   
   default_scope { order('created_at DESC') }
 end
