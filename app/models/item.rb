84d5be89b8088335f41b4a0dd4d54dfa1270f2ed
joelramsey <joel.d.ramsey@gmail.com>
2015-04-15 15:42:34 +0000

    Added days remaining to item form

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,6 +2,9 @@ class Item < ActiveRecord::Base
   belongs_to :user
   validates :name, length: {minimum: 6}, presence: true
   
+  def days_left
+      (7 - (DateTime.now.to_date - created_at.to_date)).to_i
+   end
   
   default_scope { order('created_at DESC') }
 end
