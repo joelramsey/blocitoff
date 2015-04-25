7008160143cde0f8bf1cf574bc6a1b1c019ee77f
joelramsey <joel.d.ramsey@gmail.com>
2015-04-25 22:53:49 +0000

    Modified environment.rb

--- config/environment.rb
+++ config/environment.rb
@@ -2,16 +2,6 @@
 require File.expand_path('../application', __FILE__)
 
 
-ActionMailer::Base.smtp_settings = {
-  :address        => 'smtp.sendgrid.net',
-  :port           => '587',
-  :authentication => :plain,
-  :user_name      => ENV['SENDGRID_USERNAME'],
-  :password       => ENV['SENDGRID_PASSWORD'],
-  :domain         => 'sample.com',
-  :enable_starttls_auto => true
-}
-
 # Initialize the Rails application.
 Rails.application.initialize!
 
